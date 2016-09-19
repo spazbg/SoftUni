5142ded1b7d667a96eae23a9b00615f66d994e7b
Spas Genov <spaz@mail.bg>
2016-09-19 23:03:15 +0300

    added 07. and 08.

---
+++ Complex Conditional Statements/07. Fruit Shop/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Fruit_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fruit = Console.ReadLine();
+            var dayOfWeek = Console.ReadLine();
+            var quantity = double.Parse(Console.ReadLine());
+            var price = -1.0;
+
+            if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+            {
+                if (fruit == "banana")
+                {
+                    price = 2.70;
+                }
+                else if (fruit == "apple")
+                {
+                    price = 1.25;
+                }
+                else if (fruit == "orange")
+                {
+                    price = 0.90;
+                }
+                else if (fruit == "grapefruit")
+                {
+                    price = 1.60;
+                }
+                else if (fruit == "kiwi")
+                {
+                    price = 3.00;
+                }
+                else if (fruit == "pineapple")
+                {
+                    price = 5.60;
+                }
+                else if (fruit == "grapes")
+                {
+                    price = 4.20;
+                }
+                
+            }
+            else if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+            {
+                if (fruit == "banana")
+                {
+                    price = 2.50;
+                }
+                else if (fruit == "apple")
+                {
+                    price = 1.20;
+                }
+                else if (fruit == "orange")
+                {
+                    price = 0.85;
+                }
+                else if (fruit == "grapefruit")
+                {
+                    price = 1.45;
+                }
+                else if (fruit == "kiwi")
+                {
+                    price = 2.70;
+                }
+                else if (fruit == "pineapple")
+                {
+                    price = 5.50;
+                }
+                else if (fruit == "grapes")
+                {
+                    price = 3.85;
+                }
+                
+            }
+            if (price == -1.0)
+            {
+                Console.WriteLine("error");
+            }
+            else
+            Console.WriteLine("{0:f2}", price * quantity);
+        }
+    }
+}
---
+++ Complex Conditional Statements/08. Trade Comissions/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Trade_Comissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var city = Console.ReadLine().ToLower();
+            var sales = double.Parse(Console.ReadLine());
+            var commission = 0.0;
+
+            
+            if (city == "sofia")
+            {
+                if (sales > 10000) commission = 0.12;
+                else if (sales > 1000) commission = 0.08;
+                else if (sales > 500) commission = 0.07;
+                else if (sales > 0) commission = 0.05;
+                else Console.WriteLine("error");
+            }
+            else if (city == "varna")
+            {
+                if (sales > 10000) commission = 0.13;
+                else if (sales > 1000) commission = 0.1;
+                else if (sales > 500) commission = 0.075;
+                else if (sales > 0) commission = 0.045;
+                else Console.WriteLine("error");
+            }
+            else if (city == "plovdiv")
+            {
+                if (sales > 10000) commission = 0.145;
+                else if (sales > 1000) commission = 0.12;
+                else if (sales > 500) commission = 0.08;
+                else if (sales > 0) commission = 0.055;
+                else Console.WriteLine("error");
+            }
+            if (commission == 0.0 || (city != "sofia" && city != "varna" && city != "plovdiv"))
+            {
+                Console.WriteLine("error");
+            } 
+            else
+            {
+                Console.WriteLine("{0:f2}", commission * sales);
+            }
+                
+        }
+    }
+}

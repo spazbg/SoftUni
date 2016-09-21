5a12cf4dc6ed57eb6292fcb507cb10299107c9ba
Spas Genov <spaz@mail.bg>
2016-09-21 17:12:03 +0300

    adding 10., 12., and 13.

---
+++ Complex Conditional Statements/12. Cinema/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string projectionType = Console.ReadLine();
+            int rolls = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+            var ticketPrice = 0.0;
+
+            int allSeats = rolls * columns;
+            /*
+
+            if (projectionType == "Premiere")
+            {
+                ticketPrice = 12.00;
+            }
+            else if (projectionType == "Normal")
+            {
+                ticketPrice = 7.50;
+            }
+            else if (projectionType == "Discount")
+            {
+                ticketPrice = 5.00;
+            }
+
+            Console.WriteLine("{0:f2} leva", ticketPrice*allSeats);
+            */
+
+            switch (projectionType)
+            {
+                case "Premiere":
+                    ticketPrice = 12.00;
+                    break;
+                case "Normal":
+                    ticketPrice = 7.50;
+                    break;
+                case "Discount":
+                    ticketPrice = 5.00;
+                    break;
+
+             }
+            Console.WriteLine("{0:f2} leva", ticketPrice * allSeats);
+
+        }
+    }
+}
---
+++ Complex Conditional Statements/13. Volleyball/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var yearType = Console.ReadLine();
+            int holidaysP = int.Parse(Console.ReadLine());
+            int weekendsOutH = int.Parse(Console.ReadLine());
+            double Total = 0.0;
+
+            double daysInSofia = (48 - weekendsOutH) * 3.0 / 4;
+            Total = daysInSofia + weekendsOutH + (holidaysP * (2.0 / 3));
+
+            if (yearType == "leap")
+            {
+                Total = (0.15 * Total) + Total;
+                Console.WriteLine(Math.Floor(Total));
+
+            }
+            else if (yearType == "normal")
+            {
+                Console.WriteLine(Math.Floor(Total));
+            }
+        }
+    }
+}

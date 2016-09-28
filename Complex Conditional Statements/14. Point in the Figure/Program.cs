91dd58f90557d8ca948906620de2f91b7862b144
Spas Genov <spaz@mail.bg>
2016-09-28 22:59:05 +0300

    +14, +15

---
+++ Complex Conditional Statements/14. Point in the Figure/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Point_in_the_Figure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+
+            //logika za tochka v dolnata figura
+            if ((x > h && x < 2 * h) && y == h)
+            {
+                Console.WriteLine("inside");
+            }
+            else if ((0 <= x && x <= (3 * h)) && (0 <= y && y <= h))
+            {
+                if ((0 < x && x < (3 * h)) && (0 < y && y < h))
+                {
+                    Console.WriteLine("inside");
+                }
+                else
+                {
+                    Console.WriteLine("border");
+                }
+
+            }
+            else if ((h <= x && x <= (2 * h)) && (h <= y && y <= (4 * h)))
+            {
+                if ((h < x && x < (2 * h)) && (h < y && y < (4 * h)))
+                {
+                    Console.WriteLine("inside");
+                }
+                else
+                {
+                    Console.WriteLine("border");
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("outside");
+            }
+
+        }
+    }
+}
---
+++ Complex Conditional Statements/15. ON TIME/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Coding 101 Exam - 6 March 2016
+
+namespace _03.On_Time_for_the_Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hh = int.Parse(Console.ReadLine());
+            int mm = int.Parse(Console.ReadLine());
+            int hh1 = int.Parse(Console.ReadLine());
+            int mm1 = int.Parse(Console.ReadLine());
+            int allMin = mm + hh * 60;
+            int allMin1 = mm1 + hh1 * 60;
+
+            var differenceTime = allMin1 - allMin;
+
+            if (-30 <= differenceTime && differenceTime <= 0)
+            {
+                if (differenceTime == 0)
+                {
+                    Console.WriteLine("On time");
+                }
+                else
+                {
+                    Console.WriteLine("On time");
+                    Console.WriteLine("{0} minutes before the start", Math.Abs(differenceTime));
+                }
+
+            }
+            else if (differenceTime < -30)
+            {
+                if (-60 < differenceTime && differenceTime < -30)
+                {
+                    Console.WriteLine("Early");
+                    Console.WriteLine("{0} minutes before the start", Math.Abs(differenceTime));
+                }
+                else if (differenceTime <= -60)
+                {
+                    Console.WriteLine("Early");
+                    Console.WriteLine("{0}:{1:00} hours before the start", Math.Abs(differenceTime / 60), Math.Abs(differenceTime % 60));
+                }
+
+            }
+            else if (differenceTime > 0)
+            {
+
+                if (differenceTime < 60)
+                {
+                    Console.WriteLine("Late");
+                    Console.WriteLine("{0} minutes after the start", differenceTime);
+                }
+                else
+                {
+                    Console.WriteLine("Late");
+                    Console.WriteLine("{0}:{1:00} hours after the start", differenceTime / 60, differenceTime % 60);
+                }
+            }
+
+
+        }
+    }
+}

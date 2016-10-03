e24b7f4096c8e290cdae06b51f4d066c1ff47df1
Spas Genov <spaz@mail.bg>
2016-10-03 23:31:05 +0300

    9, 16

---
+++ Simple Loops/16. Half Sum Element/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxNum = 0;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int curruntNum = int.Parse(Console.ReadLine());
+
+                sum = sum + curruntNum;
+
+                if (curruntNum > maxNum)
+                {
+                    maxNum = curruntNum;
+                }
+                
+            }
+            if (sum-maxNum == maxNum)
+            {
+                Console.WriteLine("Yes\nSum = {0}", sum - maxNum);
+            }
+            else
+            {
+                Console.WriteLine("No\nDiff = {0}", Math.Abs(sum-maxNum-maxNum));
+            }
+        }
+    }
+}
---
+++ Simple Loops/Simple Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            int strLength = str.Length;
+            int sumChar = 0;
+
+            for (int i = 0; i < strLength; i++)
+            {
+                switch (str[i])
+                {
+                    case 'a': sumChar = sumChar + 1; break;
+                    case 'e': sumChar = sumChar + 2; break;
+                    case 'i': sumChar = sumChar + 3; break;
+                    case 'o': sumChar = sumChar + 4; break;
+                    case 'u': sumChar = sumChar + 5; break;
+                }
+            }
+            Console.WriteLine(sumChar);
+        }
+    }
+}
